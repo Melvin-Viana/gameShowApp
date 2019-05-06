c72b4acb60e96268fd47a9528ed7fa813520d70d
Melvin Viana <mjviana057@gmail.com>
2019-05-05 22:06:51 -0700

    setup checkWin() function

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 
 // Elements
-const qwerty = $('#qwerty');
-const phrase = $('#phrase');
-const startBtn = $('.btn__reset');
-const phraseUl = phrase.first();
-const startClass = $('.start');
-const title = $('.title')
+const $qwerty = $('#qwerty');
+const $phrase = $('#phrase');
+const $startBtn = $('.btn__reset');
+const $phraseUl = $phrase.first();
+const $startClass = $('.start');
+const $title = $('.title')
+const $overlay = $('#overlay');
 
 // Utility Variables
 let missed = 0; //Add + 1 when player's guess missed
@@ -30,7 +31,7 @@ const addPhraseToDisplay=arr=>{
         let newChar = document.createElement('li');
         newChar.textContent=arr[i];
         //Append li to #phrase ul
-        phraseUl.append(newChar);
+        $phraseUl.append(newChar);
         //If li is not a space
         if(arr[i]!==" ")
             newChar.classList.add('letter')
@@ -53,38 +54,44 @@ const checkLetter =(key)=>{
 
 // TODO: checkWin function
 const checkWin = () =>{
-
+    const letters = document.querySelectorAll('.letter'); //Letters created only when game is started.
+    const shownLetters = document.querySelectorAll('.show');
+    if(letters===shownLetters){
+        $overlay.text("WIN");
+    }
+    if(missed===5){
+        $overlay.text("LOSE");
+    }
 }
 
+const keyboardEvent =()=>{}
 // MAIN JS CODE
 // Check if DOM is ready.
 document.addEventListener("DOMContentLoaded", () => {
 
     //Attach a event listener to the “Start Game” button to hide the start screen overlay.
-    startBtn.on('click', () => {
+    $startBtn.on('click', () => {
         const phraseArray = getRandomPhraseAsArray(phrasesArr);
         // Set the game display.
             addPhraseToDisplay(phraseArray);
-            startBtn.css('display','none');
-            startClass.css('backgroundColor','initial')
-            title.css('display','none');
-        // TODO: Add an event listener to the keyboard.
-        document.addEventListener('keypress', (event)=>{
+            $startBtn.css('display','none');
+            $startClass.css('backgroundColor','initial')
+            $title.css('display','none');
+
+            document.addEventListener('keypress', (event)=>{
             //Add button to chosen
 
-            console.log(event);
             let letterFound = checkLetter(event.key);
-            // Count the missed guesses in the game.
-                if(letterFound){
-                    //Add .chosen to button
-                    let letterBtn = $(`button:contains(${letterFound})`);
-                    letterBtn.addClass('chosen');
-                }
-
+            //Add .chosen to button
+            let letterBtn = $(`button:contains(${letterFound})`);
+            letterBtn.addClass('chosen');
+            letterBtn.prop('disabled',true);
                 //If checkLetter function returns a null value, the player has guesssed thwe wrong letter
-                else if(!letterFound)
-                missed++;
-
+                if(!letterFound)
+                {missed++;
+                    $('#scoreboard ol li').last().remove();
+                }
+            checkWin();
             });
 
     });
