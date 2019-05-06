58e535c96127bcae9e003fe2669c0a34607204f0
Melvin Viana <mjviana057@gmail.com>
2019-05-05 19:08:34 -0700

    Convert to jquery; letter button add show class

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// Helper functions
-const dcq =(selector)=>document.querySelector(selector);
+
 
 // Elements
-const qwerty = dcq('#qwerty');
-const phrase = dcq('#phrase');
-const startBtn = dcq('.btn__reset');
-const phraseUl = phrase.firstElementChild;
-const startClass = dcq('.start');
-const title = dcq('.title')
+const qwerty = $('#qwerty');
+const phrase = $('#phrase');
+const startBtn = $('.btn__reset');
+const phraseUl = phrase.first();
+const startClass = $('.start');
+const title = $('.title')
 
 // Utility Variables
 let missed = 0; //Add + 1 when player's guess missed
@@ -31,7 +30,7 @@ const addPhraseToDisplay=arr=>{
         let newChar = document.createElement('li');
         newChar.textContent=arr[i];
         //Append li to #phrase ul
-        phraseUl.appendChild(newChar);
+        phraseUl.append(newChar);
         //If li is not a space
         if(arr[i]!==" ")
             newChar.classList.add('letter')
@@ -41,16 +40,15 @@ const addPhraseToDisplay=arr=>{
 // TODO: CheckLetter function - One paramater button player has guessed
 const checkLetter =(key)=>{
     const letters = document.querySelectorAll('.letter'); //Letters created only when game is started.
-
+    let letter = null;
     // Loop through all .letter elements
     for(let i=0; i<letters.length;i++){
         //Check if they match the button
         if(letters[i].textContent===key)
-        return letters[i].textContent;
-        // If loop reaches end no match return null
-        if(i===letters.length-1 )
-            return null;
+        {   letters[i].classList.add('show');
+            letter= letters[i].textContent;}
     }
+    return letter;
 };
 
 // TODO: checkWin function
@@ -58,26 +56,35 @@ const checkWin = () =>{
 
 }
 
-// !MAIN JS CODE
+// MAIN JS CODE
 // Check if DOM is ready.
 document.addEventListener("DOMContentLoaded", () => {
 
     //Attach a event listener to the “Start Game” button to hide the start screen overlay.
-    startBtn.addEventListener('click', () => {
+    startBtn.on('click', () => {
         const phraseArray = getRandomPhraseAsArray(phrasesArr);
         // Set the game display.
             addPhraseToDisplay(phraseArray);
-            startBtn.style.display='none';
-            startClass.style.backgroundColor="initial"
-            title.style.display='none';
+            startBtn.css('display','none');
+            startClass.css('backgroundColor','initial')
+            title.css('display','none');
         // TODO: Add an event listener to the keyboard.
         document.addEventListener('keypress', (event)=>{
-            let letter = checkLetter(event.key);
+            //Add button to chosen
+
+            console.log(event);
+            let letterFound = checkLetter(event.key);
             // Count the missed guesses in the game.
+                if(letterFound){
+                    //Add .chosen to button
+                    let letterBtn = $(`button:contains(${letterFound})`);
+                    letterBtn.addClass('chosen');
+                }
+
                 //If checkLetter function returns a null value, the player has guesssed thwe wrong letter
-                if(!letter)
+                else if(!letterFound)
                 missed++;
-            console.log(missed);
+
             });
 
     });
